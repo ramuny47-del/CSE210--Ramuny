74e60be0956b67bf8cf9c3637c09876f7a23b31b
udomravylim <udomravylim@gmail.com>
2026-01-10 19:28:47 -0700

    Grade evaluation added

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -11,6 +11,6 @@ class Program
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
-        Console.WriteLine("Your name is " + last_name + ", " + first_name);
+        Console.WriteLine("Your name is " + last_name + ", " + first_name + ".");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string input = Console.ReadLine();
+        float grade = float.Parse(input);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine(letter);
+            
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine(letter);
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine(letter);
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            Console.WriteLine(letter);
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine(letter);
+        }
+        if ( letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("You have passed!");
+        }
     }
 }
